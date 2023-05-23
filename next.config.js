4d8e8dab460f5ffbee83cfa8fcb63c2c13d3a650
Your Name <you@example.com>
2023-05-23 19:45:09 -0300

    a

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
-module.exports = {
-  experimental: {
-    images: false,
-    optimizeImages: false,
-  },
-  // Otras configuraciones...
-  output: {
-    // Habilita la exportación estática
-    export: true,
-  },
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  output: 'export',
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  // trailingSlash: true,
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
 };
+ 
+module.exports = nextConfig;
